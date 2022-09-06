387b31f43b20130464b4806e5b0d46d2f4a7587b
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-09-06 10:12:01 +0400

    API request end points

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    </Routes>
+  </div>;
 }
 
 export default App;
