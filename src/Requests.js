c727cc69c66ff826d3249ba8e5a1ff0ed530cbb0
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-09-06 15:24:08 +0400

    ADD: Firebase Authentication

--- src/Requests.js
+++ src/Requests.js
@@ -1,4 +1,4 @@
-const key = "bb2cd013b060d4a698fe29f590ad5eeb";
+const key = process.env.REACT_APP_IMDB_API_KEY;
 
 const requests = {
   requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
+  const navigate = useNavigate();
+
+  const handleLogOut = async () => {
+    try {
+      await logOut();
+      navigate("/signup");
+    } catch (error) {
+      alert("Error logging out!");
+    }
+  };
+
   return (
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
-      <h1 className="text-[#E50914] text-4xl font-bold cursor-pointer">
-        NETFLIX
-      </h1>
-      <div>
-        <button className="text-white pr-4">Sign In</button>
-        <button className="bg-[#E50914] px-6 py-2 rounded cursor-pointer text-white">
-          Sign Up
-        </button>
-      </div>
+      <Link to="/">
+        <h1 className="text-[#E50914] text-4xl font-bold cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      {user?.email ? (
+        <div>
+          <Link to="/account">
+            <button className="text-white pr-4">Account</button>
+          </Link>
+          <button
+            onClick={handleLogOut}
+            className="bg-[#E50914] px-6 py-2 rounded cursor-pointer text-white"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Link to="/login">
+            <button className="text-white pr-4">Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button className="bg-[#E50914] px-6 py-2 rounded cursor-pointer text-white">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
